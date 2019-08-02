1232f8d85f1cb296b577fa1cdafeb3e14dedcf20
Jorden3 <jorden3@pdx.edu>
2019-08-02 15:16:08 -0600

    Module 15 finished

--- pipes-start/src/app/app.component.ts
+++ pipes-start/src/app/app.component.ts
@@ -32,7 +32,24 @@ export class AppComponent {
       started: new Date(15, 1, 2017)
     }
   ];
-  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+
+  filteredStatus = '';
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
+
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    });
+  }
+
+  getStatusClasses(server: { instanceType: string, name: string, status: string, started: Date }) {
     return {
       'list-group-item-success': server.status === 'stable',
       'list-group-item-warning': server.status === 'offline',
